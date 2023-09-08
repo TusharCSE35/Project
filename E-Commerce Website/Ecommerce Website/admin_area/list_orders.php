c08557c4d0629d1f16022f3e470ab4c078b8bc39
Tushar Sarkar <56844241+TusharCSE35@users.noreply.github.com>
2023-09-08 22:12:41 +0600

    Add files via upload

---
+++ E-Commerce Website/Ecommerce Website/admin_area/list_orders.php
@@ -0,0 +1,46 @@
+<h3 class="text-center text-success">All Orders</h3>
+<table class="table table-bordered mt-5">
+    <thead class="bg-info">
+        <?php
+        $get_orders="Select * from `user_orders`";
+        $result=mysqli_query($con,$get_orders);
+        $row_count=mysqli_num_rows($result);
+        if($row_count==0){
+            echo "<h2 class='text-danger text-center mt-5'>No Orders Yet</h2>";
+        }
+        else{
+            echo "<tr class='text-center'>
+            <td>Serial No</td>
+            <td>Due Amount</td>
+            <td>Invoice Number</td>
+            <td>Total Products</td>
+            <td>Order Date</td>
+            <td>Status</td>
+            <td>Delete</td>
+        </tr>
+    </thead>
+    <tbody class='bg-secondary text-light'>";
+        $number=0;
+        while($row_data=mysqli_fetch_assoc($result)){
+            $order_id=$row_data['order_id'];
+            $user_id=$row_data['user_id'];
+            $amount_due=$row_data['amount_due'];
+            $invoice_number=$row_data['invoice_number'];
+            $total_products=$row_data['total_products'];
+            $order_date=$row_data['order_date'];
+            $order_status=$row_data['order_status'];
+            $number++;
+            echo " <tr class='text-center'>
+            <td>$number</td>
+            <td>$amount_due</td>
+            <td>$invoice_number</td>
+            <td>$total_products</td>
+            <td>$order_date</td>
+            <td>$order_status</td>
+            <td><a href='' class='text-dark'><i class='fa-solid fa-trash'></i></a></td>
+            </tr>";
+            }
+        }
+        ?>
+    </tbody>
+</table>
